ab3e67253cd5b4d12eb12c4a7a4ea34f5a320ae1
Jared Scarr <jaredscarr@gmail.com>
2020-11-11 10:26:15 -0800

    Introduced Material-UI and basic Navigation bar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const NavBar = () => {
   const { authState, authService } = useOktaAuth();
   const history = useHistory();
+  const classes = useStyles();
 
   if (authState.isPending) {
     return <div>Loading...</div>;
   }
 
   const button = authState.isAuthenticated ?
-    <button onClick={() => {authService.logout()}}>Logout</button> :
-    <button onClick={() => {history.push('/login')}}>Login</button>;
+    <Button color="inherit" onClick={() => {authService.logout()}}>Logout</Button> :
+    <Button color="inherit" onClick={() => {history.push('/login')}}>Login</Button>;
 
   return (
-    <div>
-      {button}
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            House Plants
+          </Typography>
+          {authState.isAuthenticated &&
+            <div>
+              <Button color="inherit"  onClick={() => history.push("/dashboard")}>Dashboard</Button>
+              <Button color="inherit"  onClick={() => history.push("/search")}>Search</Button>
+            </div>
+          }
+          {button}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
