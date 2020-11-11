573c84eff83ab76a74ce3a176dcc894a879da5c3
Jared Scarr <jaredscarr@gmail.com>
2020-11-11 13:59:24 -0800

    Basic styles with Avatar for PlantListItem.js

--- src/components/PlantListItem.js
+++ src/components/PlantListItem.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 const PlantListItem = ({ plant, button }) => {
+  const classes = useStyles();
   const main_species = plant.data ? plant.data.main_species : plant;
 
   return (main_species.common_name ? 
-    <li>
-      <Link to={
-        {
-          pathname: "/plant",
-          state: {
-            plant: main_species,
-            button: button,
+    <div className={classes.demo}>
+      <List dense={true}>
+        <ListItem
+          button
+          component={Link}
+          to={{
+            pathname: "/plant",
+            state: {
+              plant: main_species,
+              button: button,
+            }
           }
-        }
-      }>
-        {main_species.common_name}
-      </Link>
-    </li>
+        }>
+          <ListItemAvatar>
+            <Avatar alt={main_species.common_name} src={main_species.image_url} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={main_species.common_name}
+          />
+        </ListItem>
+      </List>
+    </div>
     :
     null
   );
