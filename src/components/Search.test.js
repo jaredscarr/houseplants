3ff7cea86da31ed92283978653c1f33ff971bc42
Jared Scarr <jaredscarr@gmail.com>
2020-11-17 14:30:36 -0800

    Packages updated. Using react msw for api mocks now. Removal of nock. Search test complete.

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,7 +1,15 @@
-import React from "react"
-import { render, unmountComponentAtNode } from "react-dom"
-import { act } from "react-dom/test-utils"
-import Search from "./Search"
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+// Must wrap the component in a Router
+import { BrowserRouter as Router } from 'react-router-dom';
+// the component to test
+import Search from './Search';
+
+const TREFLE_BASE_URL = process.env.REACT_APP_BASE_URL;
 
 jest.mock('@okta/okta-react', () => ({
     useOktaAuth: () => ({
@@ -10,53 +18,77 @@ jest.mock('@okta/okta-react', () => ({
       },
       authService: {
         handleAuthentication: jest.fn(),
-        getUser: jest.fn().mockImplementation(() => Promise.resolve('1'))
+        getUser: jest.fn().mockImplementation(() => Promise.resolve(
+          {
+            "sub": "00uid4BxXw6I6TV4m0g3",
+            "name": "John Doe",
+            "nickname": "Jimmy",
+            "given_name": "John",
+            "middle_name": "James",
+            "family_name": "Doe",
+            "profile": "https://example.com/john.doe",
+            "zoneinfo": "America/Los_Angeles",
+            "locale": "en-US",
+            "updated_at": 1311280970,
+            "email": "john.doe@example.com",
+            "email_verified": true,
+            "address": {
+              "street_address": "123 Hollywood Blvd.",
+              "locality": "Los Angeles",
+              "region": "CA",
+              "postal_code": "90210",
+              "country": "US"
+            },
+            "phone_number": "+1 (xxx) xxx-xxxx"
+          }
+        ))
       }
     })
   })
 );
 
-let container = null;
+// declare which API requests to mock
+const server = setupServer(
+  // capture "GET /greeting" requests
+  rest.get(`${TREFLE_BASE_URL}/plants/search`, (req, res, ctx) => {
+    // respond using a mocked JSON body
+    return res(ctx.json({ data: [{'id': 1, 'common_name': 'palm'} ]}))
+  })
+)
 
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+// establish API mocking before all tests
+beforeAll(() => server.listen())
+// reset any request handlers that are declared as a part of our tests
+afterEach(() => server.resetHandlers())
+// clean up once the tests are done
+afterAll(() => server.close())
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("renders user data", async () => {
-  const fakePlant = {
-    "data": [],
-    "links": {
-      "self": "/api/v1/plants/search?q=cocos",
-      "first": "/api/v1/plants/search?page=1&q=cocos",
-      "last": "/api/v1/plants/search?page=1&q=cocos"
-    },
-    "meta": {
-      "total": 0
-    }
+const setup = () => {
+  const utils = render(<Router><Search /></Router>)
+  const input = utils.getByTestId('search-input')
+  return {
+    input,
+    ...utils,
   }
+}
 
-  jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(fakePlant)
-    })
-  );  
+test('Input renders value', async () => {
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: 'palm' } })
+  expect(input.value).toBe('palm')
+})
 
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<Search id="123" />, container);
-  });
-
-  expect(container.textContent).toContain(fakePlant.data);
-
-  // remove the mock to ensure tests are completely isolated
-  global.fetch.mockRestore();
-});
+test('Check response is rendered', async () => {
+  // Arrange
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: 'palm' } })
+  // expect(input.value).toBe('palm')
+  // Act
+  fireEvent.click(screen.getByTestId('search-button'))
+  // Assert
+  await waitFor(() =>
+    // getByRole throws an error if it cannot find an element
+    screen.getByText('palm')
+  )
+  expect(screen.getByText('palm'))
+})
